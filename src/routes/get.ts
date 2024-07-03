ab5be3a54288f70875ea3d405affb417e930f654
Cosmic-rare <ysolakey1116@gmail.com>
2024-07-03 18:28:53 +0900

    クラスで調べるエンドポイント作成

--- src/routes/get.ts
+++ src/routes/get.ts
@@ -46,4 +46,66 @@ route.get("/match/:id", async (c) => {
   }
 })
 
+const lWin = (d: any, event: string) => {
+  if (["volleyball", "badminton"].includes(event)) {
+    return d.l_p1 > d.h_p1
+  } else if (event == "dodgeball") {
+    return d.l_p1 > d.h_p1
+  } else if (event == "soccer") {
+    return d.l_p1 > d.h_p1
+  }
+  return true
+}
+
+route.get("/match/:class/", async (c) => {
+  const target = parseInt(await c.req.param("class"))
+  const res = []
+
+  try {
+    // let data1 = await prisma.match.findFirst({ where: { id: id as string } });
+    const data = await prisma.match.findMany()
+    data.forEach((data1) => {
+      let match: any = [[[data1["c_1"]], [data1["c_2"]]], [[data1["c_3"]], [data1["c_4"]]], [[data1["c_5"]], [data1["c_6"]]], [[], []], [[], [data1["c_7"]]], [[], []]]
+
+      for (let i = 1; i < 6; i++) {
+        const d = data1[`p_${i}`]
+  
+        const p1 = (i == 1 || i == 2) ? 4 : (i == 3) ? 5 : (i != 6) ? 6 : null
+        const p2 = [1, 3, 4].includes(i) ? 0 : 1
+        match[p1 - 1][p2] = d.applied ?
+          match[i - 1][lWin(d, data1.event) ? 0 : 1] :
+          [].concat(match[i - 1][0], match[i - 1][1])
+      }
+  
+      for (let i = 0; i < 6; i++) {
+        if (match[i][0].includes(target)) {
+          res.push({
+            id: data1.id,
+            game: i+1,
+            opponent: match[i][1],
+            data: data1
+          })
+        }
+        if (match[i][1].includes(target)) {
+          res.push({
+            id: data1.id,
+            game: i+1,
+            opponent: match[i][0],
+            data: data1
+          })
+        }
+      }
+    
+    })
+
+    
+    return c.json(res)
+  } catch (error) {
+    console.log(error)
+    return c.json({}, 500)
+  }
+})
+
+
+
 export default route
