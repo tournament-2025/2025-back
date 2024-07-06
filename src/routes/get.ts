3d8b0ad221addd679defc093a7ef698314a36e4e
Cosmic-rare <ysolakey1116@gmail.com>
2024-07-06 17:23:44 +0900

    確定が分かるように

--- src/routes/get.ts
+++ src/routes/get.ts
@@ -57,48 +57,48 @@ const lWin = (d: any, event: string) => {
   return true
 }
 
-route.get("/match/:class/", async (c) => {
-  const target = parseInt(await c.req.param("class"))
+route.get("/match/:grade/:class/", async (c) => {
+  const targetClass = parseInt(await c.req.param("class"))
+  const targetGrade = parseInt(await c.req.param("grade"))
   const res = []
 
   try {
-    // let data1 = await prisma.match.findFirst({ where: { id: id as string } });
-    const data = await prisma.match.findMany()
+    const data = await prisma.match.findMany({ where: { gread: targetGrade } })
     data.forEach((data1) => {
       let match: any = [[[data1["c_1"]], [data1["c_2"]]], [[data1["c_3"]], [data1["c_4"]]], [[data1["c_5"]], [data1["c_6"]]], [[], []], [[], [data1["c_7"]]], [[], []]]
 
       for (let i = 1; i < 6; i++) {
         const d = data1[`p_${i}`]
-  
+
         const p1 = (i == 1 || i == 2) ? 4 : (i == 3) ? 5 : (i != 6) ? 6 : null
         const p2 = [1, 3, 4].includes(i) ? 0 : 1
         match[p1 - 1][p2] = d.applied ?
           match[i - 1][lWin(d, data1.event) ? 0 : 1] :
           [].concat(match[i - 1][0], match[i - 1][1])
       }
-  
+
       for (let i = 0; i < 6; i++) {
-        if (match[i][0].includes(target)) {
+        if (match[i][0].includes(targetClass)) {
           res.push({
             id: data1.id,
-            game: i+1,
+            game: i + 1,
             opponent: match[i][1],
-            data: data1
+            data: data1,
+            certainty: match[i][1].length == 1 ? true : false,
           })
         }
-        if (match[i][1].includes(target)) {
+        if (match[i][1].includes(targetClass)) {
           res.push({
             id: data1.id,
-            game: i+1,
+            game: i + 1,
             opponent: match[i][0],
-            data: data1
+            data: data1,
+            certainty: match[i][0].length == 1 ? true : false,
           })
         }
       }
-    
     })
 
-    
     return c.json(res)
   } catch (error) {
     console.log(error)
