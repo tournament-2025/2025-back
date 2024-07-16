86915f8b08b8dc93d82570852961c7492ef2d272
Cosmic-rare <ysolakey1116@gmail.com>
2024-07-16 20:04:23 +0900

    seed消す

--- src/routes/seed.ts
+++ src/routes/seed.ts
@@ -5,13 +5,13 @@ import data from "./data/sr"
 let prisma = new PrismaClient()
 const route = new Hono()
 
-route.get("/", async (c) => {
-  const r = await prisma.match.createMany({
-    // @ts-ignore
-    data: data
-  })
-  return await c.json({ ok: true, data: r }, 200)
-})
+// route.get("/", async (c) => {
+//   const r = await prisma.match.createMany({
+//     // @ts-ignore
+//     data: data
+//   })
+//   return await c.json({ ok: true, data: r }, 200)
+// })
 
 
 export default route
